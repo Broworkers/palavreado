d4292bb6a170409c5a9181b672ae46b650de4aa1
Daniel Libanori <daniel@current.com.br>
2011-09-22 00:00:22 -0300

    Checador do dicinário

---
+++ check.rb
@@ -0,0 +1,45 @@
+# encoding: utf-8
+
+require 'yaml'
+require 'test/unit/assertions'
+
+include MiniTest::Assertions
+
+def verify(value, klass=String)
+  raise unless klass === value
+end
+
+out = STDOUT
+err = STDERR
+file = ARGV.first
+
+out.print "Analisando #{file}... "
+begin
+  Words = YAML.load_file(file)
+
+  Words.each do |key, values|
+    $key = key
+
+    verify key
+
+    values['syllabes'].tap do |syllabes|
+      verify syllabes, Array
+      syllabes.each {|syllabe| verify syllabe }
+    end
+
+    verify values['stress'], Integer
+    verify values['description']
+
+    values['examples'].tap do |examples|
+      verify examples, Array
+      examples.each {|example| verify example}
+    end
+  end
+rescue => ex
+  out.puts "ERRO"
+  err.puts "Palavra '#$key' inválida"
+  exit 1
+else
+  out.puts "OK"
+  exit 0
+end
